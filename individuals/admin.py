bd7452dc27d1da3aa4633f9cba2df115ba77f216
Saida-Alam-Mukti <saidaalammukti15@gmail.com>
2026-04-16 01:02:13 +0600

    Update admin.py

--- individuals/admin.py
+++ individuals/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import IndividualBudget,IndividualTransaction
 
-# Register your models here.
+admin.site.register(IndividualBudget)
+admin.site.register(IndividualTransaction)
